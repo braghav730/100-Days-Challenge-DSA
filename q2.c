a2dd30614fe8b708e2f23c9f9d997af3827cd7a7
braghav730 <braghav730@gmail.com>
2026-02-01 17:33:07 +0530

    Add files via upload

---
+++ q2.c
@@ -0,0 +1,55 @@
+/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.*/
+#include <stdlib.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int offset = 100000;             
+    int hash[200001];
+
+    for (int i = 0; i < 200001; i++)
+        hash[i] = -1;
+
+    for (int i = 0; i < numsSize; i++) {
+        int complement = target - nums[i];
+        if (complement >= -100000 && complement <= 100000 &&
+            hash[complement + offset] != -1) {
+
+            int* result = (int*)malloc(2 * sizeof(int));
+            result[0] = hash[complement + offset];
+            result[1] = i;
+            *returnSize = 2;
+            return result;
+        }
+        hash[nums[i] + offset] = i;
+    }
+
+    *returnSize = 0;
+    return NULL;
+}
