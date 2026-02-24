f581f42c1351759f5fbff99ed9de82d36b6cd3e3
braghav730 <braghav730@gmail.com>
2026-02-24 14:36:30 +0530

    Add files via upload

---
+++ q26.c
@@ -0,0 +1,101 @@
+/*Problem: Delete First Occurrence of a Key - Implement using linked list with dynamic memory allocation.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+- Third line: integer key
+
+Output:
+- Print the linked list elements after deletion, space-separated
+
+Example:
+Input:
+5
+10 20 30 40 50
+30
+
+Output:
+10 20 40 50
+
+Explanation:
+Traverse list, find first node with key, remove it by adjusting previous node's next pointer.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition of the node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create linked list
+struct Node* createList(int n) {
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+    int value;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+    return head;
+}
+
+// Function to delete first occurrence of key
+struct Node* deleteFirstOccurrence(struct Node* head, int key) {
+    struct Node *temp = head, *prev = NULL;
+
+    // If head node contains the key
+    if (temp != NULL && temp->data == key) {
+        head = temp->next;
+        free(temp);
+        return head;
+    }
+
+    // Search for the key
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    // If key found
+    if (temp != NULL) {
+        prev->next = temp->next;
+        free(temp);
+    }
+
+    return head;
+}
+
+// Function to print linked list
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+int main() {
+    int n, key;
+    struct Node* head = NULL;
+
+    scanf("%d", &n);
+    head = createList(n);
+
+    scanf("%d", &key);
+    head = deleteFirstOccurrence(head, key);
+
+    printList(head);
+    return 0;
+}
