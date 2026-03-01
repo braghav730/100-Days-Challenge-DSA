2f7009dd039c5922eb9d2e722da2a72bb0942b50
braghav730 <braghav730@gmail.com>
2026-03-01 19:09:48 +0530

    Add files via upload

---
+++ q31.c
@@ -0,0 +1,100 @@
+/*Problem: Rotate Linked List Right by k Places - Implement using linked list with dynamic memory allocation.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+- Third line: integer k
+
+Output:
+- Print the linked list elements after rotation, space-separated
+
+Example:
+Input:
+5
+10 20 30 40 50
+2
+
+Output:
+40 50 10 20 30
+
+Explanation:
+Connect last node to head forming circular list. Traverse to (n-k)th node, set next to NULL, update head to (n-k+1)th node.*/
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+struct Node* createList(int n) {
+    struct Node *head = NULL, *temp = NULL, *newNode;
+    int val;
+
+    for (int i = 0; i < n; i++) {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        scanf("%d", &val);
+        newNode->data = val;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = temp->next;
+        }
+    }
+    return head;
+}
+
+struct Node* rotateRight(struct Node* head, int k) {
+    if (head == NULL || head->next == NULL || k == 0)
+        return head;
+
+    struct Node *temp = head;
+    int count = 1;
+
+    while (temp->next != NULL) {
+        temp = temp->next;
+        count++;
+    }
+
+    k = k % count;
+    if (k == 0)
+        return head;
+
+    temp->next = head;
+
+    temp = head;
+    for (int i = 1; i < count - k; i++) {
+        temp = temp->next;
+    }
+
+    head = temp->next;
+    temp->next = NULL;
+
+    return head;
+}
+
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+int main() {
+    int n, k;
+    scanf("%d", &n);
+
+    struct Node* head = createList(n);
+
+    scanf("%d", &k);
+
+    head = rotateRight(head, k);
+
+    printList(head);
+
+    return 0;
+}
